40713668ffa081dc65112adafd955fed0ac21e93
João Baptista <joaopedromotabaptista2003@gmail.com>
2025-04-20 17:46:50 +0100

    Revert "logs"
    This reverts commit 3795a7cce3e2acf2f90fba5c594e56df313f8232.

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -7,19 +7,7 @@ if (!MONGODB_URI) {
 }
 
 export async function connectDB() {
-  console.log('connectDB: Attempting to connect to MongoDB...'); // Log before checking connection state
+  if (mongoose.connection.readyState >= 1) return;
 
-  if (mongoose.connection.readyState >= 1) {
-    console.log('connectDB: MongoDB already connected.'); // Log if already connected
-    return;
-  }
-
-  try {
-    const db = await mongoose.connect(MONGODB_URI);
-    console.log('connectDB: MongoDB connected successfully.'); // Log on successful connection
-    return db;
-  } catch (error) {
-    console.error('connectDB: MongoDB connection error:', error); // Log any connection errors
-    throw error; // Re-throw the error to be caught by the caller
-  }
+  return mongoose.connect(MONGODB_URI);
 }
