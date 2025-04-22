8e957201213f45ca2b4db1de1d6d97e0cf4ff15c
João Baptista <joaopedromotabaptista2003@gmail.com>
2025-04-22 19:24:25 +0100

    test

--- src/data/getMenu.ts
+++ src/data/getMenu.ts
@@ -2,7 +2,7 @@ import fs from 'fs/promises';
 import path from 'path';
 
 export async function getMenuData(id: string) {
-  const filePath = path.join('public', 'data', id, 'menu.json');
+  const filePath = path.join('data', id, 'menu.json');
   try {
     const jsonData = await fs.readFile(filePath, 'utf-8');
     return JSON.parse(jsonData);
